41389d6806178ff46d6ade1402969acf273514b5
Fahime <fahim.mohammad.siam@gmail.com>
2023-11-06 00:36:09 +0600

    Created a post method to store user information at mongoDB database collection

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 const express = require("express");
 const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
@@ -22,16 +23,21 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // database&collection
+    const usersCollection = client.db("TasteHarmonyCafeDB").collection("users");
+
+    // post method for users
+    app.post("/users", async (req, res) => {
+      const user = req.body;
+      const result = await usersCollection.insertOne(user);
+    });
+
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
