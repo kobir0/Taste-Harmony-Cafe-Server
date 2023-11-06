c4c06b7f9603d13a535c69f0627c0119c4a3657f
Fahime <fahim.mohammad.siam@gmail.com>
2023-11-06 15:06:12 +0600

    Created a post method for all food items to store on database

--- index.js
+++ index.js
@@ -25,11 +25,38 @@ async function run() {
   try {
     // database&collection
     const usersCollection = client.db("TasteHarmonyCafeDB").collection("users");
+    const foodCollection = client
+      .db("TasteHarmonyCafeDB")
+      .collection("foodItems");
 
     // post method for users
     app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await usersCollection.insertOne(user);
+    });
+
+    // post method for addFoodItem
+    foodCollection.createIndex({ foodName: 1 }, { unique: true });
+    app.post("/addfood", async (req, res) => {
+      const newFoodItem = req.body;
+      const existingFoodItem = await foodCollection.findOne({
+        name: newFoodItem.foodName,
+      });
+      if (existingFoodItem) {
+        return res.status(400).json({ message: "Food item already exists" });
+      } else {
+        const result = await foodCollection.insertOne(newFoodItem);
+        if (result.insertedCount === 1) {
+          res.send(result);
+          return res
+            .status(201)
+            .json({ message: "Food item added successfully" });
+        } else {
+          return res
+            .status(500)
+            .json({ message: "An error occurred while adding the food item" });
+        }
+      }
     });
 
     console.log(
