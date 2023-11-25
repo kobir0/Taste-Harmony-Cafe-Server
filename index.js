a575cda13788af42cb187c21cf1bb3a5e9fa1b1e
Kobirul Islam <kobirulislam@My-MAC.local>
2023-11-25 17:14:37 +0600

    init commit

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 // middleware
 app.use(cors());
